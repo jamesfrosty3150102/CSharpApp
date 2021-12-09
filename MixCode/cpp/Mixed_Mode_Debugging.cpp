672a7136a913b2b6ff35f75af289bceb9a262772
ywang43 <james.frosty3150102@gmail.com>
2021-12-09 21:47:36 +0800

    C#,cpp mixed

---
+++ MixCode/cpp/Mixed_Mode_Debugging.cpp
@@ -0,0 +1,11 @@
+// Mixed_Mode_Debugging.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include "Mixed_Mode.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
---
+++ MixCode/cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Mixed_Mode_Calling_App
+{
+    class Program
+    {
+        [DllImport(@"D:\CSharp\MixCode\Mixed_Mode_Debugging\Debug\Mixed_Mode_Debugging.dll", EntryPoint = "mixed_mode_multiply", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Multiply(int x, int y);
+        static void Main(string[] args)
+        {
+            int result = Multiply(7, 7);
+            Console.WriteLine("The answer is {0}", result);
+            Console.ReadKey();
+       }
+    }
+}
